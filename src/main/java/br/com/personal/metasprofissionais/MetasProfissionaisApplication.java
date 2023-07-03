a7937af4e6e7f202e3c03864bb276b211ac27680
maarcosvsv <maarcosvsv@gmail.com>
2023-07-02 22:50:42 -0300

    First commit of metas-profissionais-app, including base of backend spring boot service.

---
+++ src/main/java/br/com/personal/metasprofissionais/MetasProfissionaisApplication.java
@@ -0,0 +1,14 @@
+package br.com.personal.metasprofissionais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MetasProfissionaisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetasProfissionaisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/personal/metasprofissionais/MetasProfissionaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.personal.metasprofissionais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetasProfissionaisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
